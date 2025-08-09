1af2d394c695f5fb9341ee51e25c53a6f175b8ab
Sushentsev-Vladimir-Denisovich <cool.ya-vovas@yandex.ru>
2025-08-09 19:17:53 +0500

    Add voice and video_note handlers

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import os
 from dotenv import load_dotenv
 import telebot
 
-# Замените на ваш токен
-BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+load_dotenv()
+# Считывание токена из файла .env
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 if not BOT_TOKEN:
     print("Error: TELEGRAM_BOT_TOKEN environment variable not set.")
     exit(1)
@@ -11,10 +12,20 @@ if not BOT_TOKEN:
 # Инициализация бота
 bot = telebot.TeleBot(BOT_TOKEN)
 
-# Обработчик команд (пример)
+# Обработчик команды "/start"
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     bot.reply_to(message, "Привет! Я бот для конвертации голосовых и видео сообщений в текст.  Когда я получу аудио- или видео-сообщение я представлю тебе его в текстовом формате. Также, ты можешь настроить меня, используя ...")
+
+# Обработчик голосовых сообщений
+@bot.message_handler(content_types=['voice'])
+def handle_voice(message):
+    bot.reply_to(message, "voice")
+
+# Обработчик видео сообщений (кружки)
+@bot.message_handler(content_types=['video_note'])
+def handle_video(message):
+    bot.reply_to(message, "video_note")
 
 # Запуск бота
 bot.infinity_polling()
